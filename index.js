b6bd5df9d64f5119031a48258782d015e694e3c9
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2024-08-19 08:23:33 +0700

    haii

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
-const port = 8000;
+const axios = require("axios");
 
-app.get("/", async (req, res) => {
-  res.json({
-    say: "Hello World!",
-  });
+const app = express();
+
+// Use environment variables
+const PORT = process.env.PORT || 8000;
+const apiUrl = process.env.API_URL;
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+// Function to send requests
+function sendRequest() {
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      console.log("Request successful:", response.data);
+    })
+    .catch((error) => {
+      console.error("Request failed:", error);
+    });
+}
+
+// Send a request immediately upon server start
+sendRequest();
+
+// Set interval to send request every 5 minutes (300000 ms)
+setInterval(sendRequest, process.env.REQUEST_INTERVAL);
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
